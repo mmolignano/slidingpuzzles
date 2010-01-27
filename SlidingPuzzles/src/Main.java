d7d48409777a903c8767aff5b3f659742813dd83
liukangchao <liukangchao@eac89e32-08f1-11df-beaf-bb271b2086c6>
2010-01-27 02:28:22 +0000

    main

--- SlidingPuzzles/src/Main.java
+++ SlidingPuzzles/src/Main.java
@@ -2,8 +2,23 @@ import java.util.ArrayList;
 
 
 public class Main {
-	public static void main(String []args) {
-		Board b = new Board((byte)5);
+	public static void main(String [] args) {
+		if (args.length == 0 || args.length!= 2 || args.length !=3 || args[0].equals("help")) {
+			System.out.println("Usage:");
+			System.out.println("java -Xmx2000m -jar npuzzle.jar (BOARD_SIZE) (FILENAME) [--best | --fast]");
+			System.out.println("Defaults to fast mode if one of best and fast is not selected.");
+			System.out.println("The first argument after java allocates the JVM heap space.  Give it as much as you can!");
+			return;
+		}
+		
+		int size = Integer.parseInt(args[0]);
+		String filename = args[1];
+		boolean fast = true;
+		if (args[2].equals("--best")) {
+			fast = false;
+		}
+
+		Board b = new Board((byte)size);
 		
 		if (b.size > 4) {
 			try {
@@ -17,17 +32,40 @@ public class Main {
 			}
 		}
 		
-		b.parseFile("R:\\workspace\\Coolthings\\src\\puzzles\\24-2");
+		b.parseFile(filename);
 		Runtime r = Runtime.getRuntime();
 		System.out.println("Max memory allotted: " + (r.totalMemory() / 1000000.0) + "MB");
 		
 		Heuristic h = new ManhattanDistance();
 		
 		AStarPriority alg = new AStarPriority(h);
-		String answer = alg.run(b,1, 200);
+		int scale = 1;
+		if(fast) {
+			if (size == 4) {
+				scale = 3;
+			} else if (size == 5) {
+				scale = 8;
+			}
+		}
+		/*
+		 * Max number of moves in optimal solution for
+		 * 8 puzzle: 31
+		 * 15 puzzle: 80
+		 * 24 puzzle: assumed to be 200
+		 */
+		int maxDepth = 31;
+		if (size == 4) {
+			maxDepth = 80;
+		} else if (size == 5) {
+			maxDepth = 200;
+		}
+		
+		String answer = alg.run(b, scale, maxDepth);
 		
 		if (answer != null) {
 			System.out.println("Answer: " + answer);
+		} else {
+			System.out.println("It seems there was no solution within a depth of " + maxDepth);
 		}
 		
 	}
