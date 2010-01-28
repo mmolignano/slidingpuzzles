da260d9bd344675abcca3c506b5c2e1b7fac1418
dairui8772 <dairui8772@eac89e32-08f1-11df-beaf-bb271b2086c6>
2010-01-28 00:45:23 +0000

    +comment

--- trunk/SlidingPuzzles/src/Direction.java
+++ trunk/SlidingPuzzles/src/Direction.java
@@ -1,4 +1,8 @@
-
+/**
+ * Direction enum of UP, DOWN, LEFT, and RIGHT 
+ * @author Thomas Liu (kangchao@wpi.edu), Rui Dai (ruidai@wpi.edu) Michael Molignano (mikem@wpi.edu), Kevin Nolan (knolan@wpi.edu)
+ *
+ */
 public enum Direction {
 	UP, DOWN, LEFT, RIGHT;
 	
